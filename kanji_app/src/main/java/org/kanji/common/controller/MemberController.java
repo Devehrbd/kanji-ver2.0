ccbe263bb6ca32d728fb9df874cf8b072177923c
J-main <J-main@J-018.mshome.net>
2022-12-12 18:06:22 +0900

    22/12/12

--- kanji_app/src/main/java/org/kanji/common/controller/MemberController.java
+++ kanji_app/src/main/java/org/kanji/common/controller/MemberController.java
@@ -1,28 +1,61 @@
 package org.kanji.common.controller;
 
-import java.util.Optional;
+import java.net.URLEncoder;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.kanji.member.entity.Member;
 import org.kanji.member.service.MemberServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.AllArgsConstructor;
-
 @Controller
 @RequestMapping("/member")
-@AllArgsConstructor
 public class MemberController {
 	
+	@Autowired
 	private MemberServiceImpl mService;
 	
+	private String N_CLIENT_ID = "APU1t3m4Obdif6jYJRSR";
+	
+	private String G_CLIENT_ID = "866792557950-qkcmi3hr3bop38te6t6erivdhud0ov39.apps.googleusercontent.com";
+
+	private String K_CLIENT_ID = "5af9654538cb7e4fb7145ffb2389bc71";
+	
 	@GetMapping("/loginPage")
-	public void loginPage() {
+	public void loginPage(HttpSession session, Model model) throws Exception {
+		
+		//naver
+		String N_redirectURI = URLEncoder.encode("http://localhost:8080/social/naver", "UTF-8");
+		String N_state = UUID.randomUUID().toString();
+		String N_loginURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+		N_loginURL += String.format("&client_id=%s&redirect_uri=%s&state=%s", N_CLIENT_ID, N_redirectURI, N_state);
+		session.setAttribute("N_state", N_state);
+		model.addAttribute("N_loginURL", N_loginURL);
+		
+		
+		//google
+		String G_redirectURI = URLEncoder.encode("http://localhost:8080/social/google", "UTF-8");
+		String G_state = UUID.randomUUID().toString();
+		String G_loginURL = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code";
+		String G_scope = "https://www.googleapis.com/auth/userinfo.profile";
+		G_loginURL += String.format("&client_id=%s&redirect_uri=%s&scope=%s&state=%s", G_CLIENT_ID, G_redirectURI, G_scope, G_state);
+		session.setAttribute("G_state", G_state);
+		model.addAttribute("G_loginURL", G_loginURL);
+		
+		//kakao
+		String K_redirectURI = URLEncoder.encode("http://localhost:8080/social/kakao", "UTF-8");
+		String K_state = UUID.randomUUID().toString();
+		String K_loginURL = "https://kauth.kakao.com/oauth/authorize?response_type=code";
+		K_loginURL += String.format("&client_id=%s&redirect_uri=%s&state=%s", K_CLIENT_ID, K_redirectURI, K_state);
+		session.setAttribute("K_state", K_state);
+		model.addAttribute("K_loginURL", K_loginURL);
 		
 	}
 	
@@ -73,6 +106,6 @@ public class MemberController {
 	public String logout(HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		session.invalidate();
-		return "/member/loginPage";
+		return "redirect:/member/loginPage";
 	}
 }
--- kanji_app/src/main/java/org/kanji/common/controller/SocialController.java
+++ kanji_app/src/main/java/org/kanji/common/controller/SocialController.java
@@ -1,11 +1,20 @@
 package org.kanji.common.controller;
 
 import java.io.BufferedReader;
-
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -19,11 +28,16 @@ import org.kanji.member.service.MemberServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.nimbusds.jwt.JWT;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 
 
@@ -38,23 +52,15 @@ public class SocialController {
 	private MemberServiceImpl mService;
 	private String N_CLIENT_ID = "APU1t3m4Obdif6jYJRSR";
 	private String N_CLI_SECRET = "j1QvEmBdBb";
-
-	@RequestMapping("/naverLogin")
-	public void getAuthorizationUrl(HttpSession session, Model model) throws Exception{
-		
-		String redirectURI = URLEncoder.encode("http://localhost:8080/social/naver", "UTF-8");
-		String state = UUID.randomUUID().toString();
-		String loginURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
-		loginURL += String.format("&client_id=%s&redirect_uri=%s&state=%s", N_CLIENT_ID, redirectURI, state);
-		session.setAttribute("state", state);
-		model.addAttribute("loginURL", loginURL);
-	
-	}
+	private String G_CLIENT_ID = "866792557950-qkcmi3hr3bop38te6t6erivdhud0ov39.apps.googleusercontent.com";
+	private String G_CLI_SECRET = "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";
+	private String K_CLIENT_ID = "5af9654538cb7e4fb7145ffb2389bc71";
+	private String K_CLI_SECRET = "EtT52bLhgw9UfPOqfDrXCHzza8YIXikY";
 	
 	@RequestMapping("/naver")
 	public String naver(HttpSession session, @RequestParam String code, @RequestParam String state) throws Exception{
 		
-		String sessionState = (String)session.getAttribute("state");
+		String sessionState = (String)session.getAttribute("N_state");
 		if(sessionState.equals(state)) {
 			String tempTokenURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code";
 			tempTokenURL += String.format("&client_id=%s&client_secret=%s&code=%s&state=%s",N_CLIENT_ID,N_CLI_SECRET,code,state);
@@ -101,4 +107,163 @@ public class SocialController {
 		return  "redirect:/main";
 	}
 	
+	@RequestMapping("/google")
+	public String google(HttpSession session, @RequestParam String code, @RequestParam String state) throws Exception{
+		
+		String sessionState = (String)session.getAttribute("G_state");
+		if(sessionState.equals(state)) {
+			String G_redirectURI = URLEncoder.encode("http://localhost:8080/social/google", "UTF-8");
+			String tokenParam = String.format("client_id=%s&client_secret=%s&code=%s&state=%s&grant_type=authorization_code&redirect_uri=%s",G_CLIENT_ID,G_CLI_SECRET,code,state,G_redirectURI);
+			
+			URL tokenURL = new URL("https://oauth2.googleapis.com/token");
+			HttpURLConnection con = (HttpURLConnection)tokenURL.openConnection();
+			
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			con.setRequestProperty("Content-Length", Integer.toString(tokenParam.length()));
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			
+			try (DataOutputStream dos = new DataOutputStream(con.getOutputStream())) {
+				dos.writeBytes(tokenParam);
+	        }
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+			StringBuilder sb = new StringBuilder();
+			String inputLine;
+			
+			while ((inputLine=br.readLine()) != null) {
+				sb.append(inputLine);
+			}
+	
+			String token = sb.toString();
+			
+			JSONParser parser = new JSONParser();
+			JSONObject object = (JSONObject) parser.parse(token);
+			
+			String tempProfileURL =  "https://www.googleapis.com/oauth2/v1/userinfo?alt=json";
+			String headerProfileURL = "Bearer " + object.get("access_token");
+			
+			URL profileURL = new URL(tempProfileURL);
+			HttpURLConnection httpURL = (HttpURLConnection) profileURL.openConnection();
+			
+			httpURL.setRequestProperty("Authorization", headerProfileURL);
+			httpURL.setRequestProperty("aceept", "application/json");
+			
+			BufferedReader br2 = new BufferedReader(new InputStreamReader(httpURL.getInputStream(),"UTF-8"));
+			
+			StringBuilder sb2 = new StringBuilder();
+			String inputLine2;
+			
+			while ((inputLine2=br2.readLine()) != null) {
+				sb2.append(inputLine2);
+
+			}
+			
+			String profileData = sb2.toString();
+			JSONObject object2 = (JSONObject) parser.parse(profileData);
+			
+			Member member = new Member();
+			member.setMemberId(String.valueOf(object2.get("id")));
+			member.setLoginMethod("GOOGLE");
+			
+			session.setAttribute("login_member_id",member.getMemberId());
+			session.setAttribute("login_member", member);
+			
+			if(mService.getMember(member).isEmpty()) {
+				mService.joinMember(member);
+			}
+			
+			return "redirect:/course/select";
+			
+		}
+		
+		return  "redirect:/main";
+	}
+	
+	@RequestMapping("/kakao")
+	public String kakao(HttpSession session, @RequestParam String code, @RequestParam String state) throws Exception{
+		
+		String sessionState = (String)session.getAttribute("K_state");
+		if(sessionState.equals(state)) {
+			String K_redirectURI = URLEncoder.encode("http://localhost:8080/social/kakao", "UTF-8");
+			String tokenParam = String.format("client_id=%s&client_secret=%s&code=%s&state=%s&grant_type=authorization_code&redirect_uri=%s",K_CLIENT_ID,K_CLI_SECRET,code,state,K_redirectURI);
+			
+			URL tokenURL = new URL("https://kauth.kakao.com/oauth/token");
+			HttpURLConnection con = (HttpURLConnection)tokenURL.openConnection();
+			
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+			con.setRequestProperty("Content-Length", Integer.toString(tokenParam.length()));
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			
+			try (DataOutputStream dos = new DataOutputStream(con.getOutputStream())) {
+				dos.writeBytes(tokenParam);
+	        }
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+			StringBuilder sb = new StringBuilder();
+			String inputLine;
+			
+			while ((inputLine=br.readLine()) != null) {
+				sb.append(inputLine);
+			}
+	
+			String token = sb.toString();
+			
+			JSONParser parser = new JSONParser();
+			JSONObject object = (JSONObject) parser.parse(token);
+
+			String tempProfileURL =  "https://kapi.kakao.com/v2/user/me";
+			String headerProfileURL = "Bearer " + object.get("access_token");
+			
+			URL profileURL = new URL(tempProfileURL);
+			HttpURLConnection httpURL = (HttpURLConnection) profileURL.openConnection();
+			
+			httpURL.setRequestProperty("Authorization", headerProfileURL);
+			httpURL.setRequestProperty("aceept", "application/json");
+			
+			BufferedReader br2 = new BufferedReader(new InputStreamReader(httpURL.getInputStream(),"UTF-8"));
+			
+			StringBuilder sb2 = new StringBuilder();
+			String inputLine2;
+			
+			while ((inputLine2=br2.readLine()) != null) {
+				sb2.append(inputLine2);
+
+			}
+			
+			String profileData = sb2.toString();
+			JSONObject object2 = (JSONObject) parser.parse(profileData);
+			
+			System.out.println(object2.get("id")+"여기");
+			
+			Member member = new Member();
+			member.setMemberId(String.valueOf(object2.get("id")));
+			member.setLoginMethod("KAKAO");
+			
+			session.setAttribute("login_member_id", member.getMemberId());
+			session.setAttribute("login_member", member);
+			
+			if(mService.getMember(member).isEmpty()) {
+				mService.joinMember(member);
+			}
+			
+			return "redirect:/course/select";
+			
+			
+//			String encodeToken = ((String)object.get("id_token")).replace("\n","").replace("-", "+").replace("_", "/");	
+//			byte[] a = Base64.getMimeDecoder().decode(encodeToken);
+//			
+//			
+//			System.out.println(new String(a, StandardCharsets.UTF_8));
+		
+			
+		}
+		
+		return  "redirect:/main";
+	}
 }
